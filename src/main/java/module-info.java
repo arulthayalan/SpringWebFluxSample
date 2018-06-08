4214a52d3772590c2d57376cd715c9f26d3bbad6
Arul Geroge <arul.george@auspost.com.au>
2018-06-08 14:56:54 +1000

    first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.webflux {
+    requires reactor.core;
+    requires spring.web;
+    requires spring.context;
+    requires spring.webflux;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.boot.starter.data.mongodb.reactive;
+}
