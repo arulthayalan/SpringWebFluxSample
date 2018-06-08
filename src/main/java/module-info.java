5a8837b24013fed5b205abfdb9257609790164bb
Arul Geroge <arul.george@auspost.com.au>
2018-06-08 16:02:53 +1000

    remove module info

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.webflux {
-    requires reactor.core;
-    requires spring.web;
-    requires spring.context;
-    requires spring.webflux;
-    requires spring.boot;
-    requires spring.boot.autoconfigure;
-    requires spring.boot.starter.data.mongodb.reactive;
-}
